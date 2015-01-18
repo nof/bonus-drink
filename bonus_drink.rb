4e5541dad2ac7e1e67ce655c5a7224bca1b2a93b
nof <sakape@gmail.com>
2015-01-18 17:21:16 +0900

    answer

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,16 @@
 class BonusDrink
+  BASE_OF_BONUS = 3
+
   def self.total_count_for(amount)
-    0
+    amount + bonus_count_for(amount)
+  end
+
+  def self.bonus_count_for(amount)
+    return 0 if amount < BASE_OF_BONUS
+
+    quot = amount / BASE_OF_BONUS
+    rem = amount % BASE_OF_BONUS 
+    new_bottole_count = quot
+    new_bottole_count + bonus_count_for(quot + rem)
   end
 end
